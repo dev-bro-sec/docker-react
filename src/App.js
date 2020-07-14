da2c6547633f3050978993c06beec205baff56b0
Regev-Agabi <regev21ar@gmail.com>
2020-07-14 20:40:23 +0000

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            This is demo Deployment 
+            change on the deature branch 
         </p>
         <a
           className="App-link"
